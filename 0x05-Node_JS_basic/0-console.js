3d58cfd0c430a15832ab51e30ddcaf7c62770c19
jooon2994 <wizyoni7@gmail.com>
2023-02-23 22:21:12 +0300

    new

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+// Betty style comment to begin with
+// function to display message
+function displayMessage(message) {
+  console.log(message);
+}
+
+// Example usage:
+displayMessage("Hello, world!");
+
